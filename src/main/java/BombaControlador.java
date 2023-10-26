ec2a174e896169adea871a6788e8bfba82c73641
Jose Zacarias <elzackarias@gmail.com>
2023-10-25 23:28:33 -0600

    v1

---
+++ src/main/java/BombaControlador.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BombaControlador implements ActionListener {
+    private BombaModelo modelo;
+    private BombaVista vista;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
